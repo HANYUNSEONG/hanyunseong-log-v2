3ec5df70092cd601585be2c826cf593f94a88f68
HANYUNSEONG <hanyunseong.dev@gmail.com>
2023-03-21 00:24:28 +0900

    feat: 홈 화면 post 렌더링

--- src/components/posts/PostItem/PostItem.tsx
+++ src/components/posts/PostItem/PostItem.tsx
@@ -1,15 +1,25 @@
-const PostItem = () => {
+import { Post } from "@/types/post";
+import dayjs from "dayjs";
+
+const PostItem = ({ frontMatter }: Post) => {
+  const { title, description, date, tags } = frontMatter;
+
   return (
     <article className="flex justify-end align-center gap-x-4 flex-1 gap-y-2 flex-col">
-      <h2 className="font-bold text-xl">pnpm으로 Next.js 세팅을 해보자</h2>
-      <p className="text-sm">
-        내 프로젝트를 세팅하는데 여러 패키지 매니저를 사용해보고 싶었다.내
-        프로젝트를 세팅하는데 여러 패키지 매니저를 사용해보고 싶었다.내
-        프로젝트를 세팅하는데 여러 패키지 매니저를 사용해보고 싶었다.내
-        프로젝트를 세팅하는데 여러 패키지 매니저를 사용해보고 싶었다.내
-        프로젝트를 세팅하는데 여러 패키지 매니저를 사용해보고 싶었다.
+      <h2 className="font-bold text-xl">{title}</h2>
+      <p className="text-sm">{description}</p>
+      <ul className="flex gap-1">
+        {tags.map((tag) => (
+          <li key={`${title}-${tag}`}>
+            <span className="p-1.5 text-xs rounded bg-slate-500 text-white">
+              {tag}
+            </span>
+          </li>
+        ))}
+      </ul>
+      <p className="text-xs mt-2 text-gray-700">
+        {dayjs(date).format("YYYY.MM.DD")}
       </p>
-      <p className="text-xs mt-2 text-gray-700">2022.03.16</p>
     </article>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,26 @@
 import PostItem from "@/components/posts/PostItem";
+import { Post } from "@/types/post";
 import { getAllPosts } from "@/utils/post";
 
-const Home = () => {
+type Props = {
+  posts: Post[];
+};
+
+const Home = ({ posts }: Props) => {
   return (
     <article className="py-8">
       <div>
         <h1 className="text-lg border-b border-gray-200 py-4">전체 글</h1>
       </div>
       <ul className="mt-5 flex flex-col gap-y-4">
-        <li className="bg-white p-5 rounded-lg shadow-sm">
-          <PostItem />
-        </li>
-        <li className="bg-white p-5 rounded-lg shadow-sm">
-          <PostItem />
-        </li>
+        {posts?.map((post) => (
+          <li
+            className="bg-white p-5 rounded-lg shadow-sm cursor-pointer transition hover:-translate-y-1 hover:shadow-md"
+            key={post.frontMatter.slug}
+          >
+            <PostItem {...post} />
+          </li>
+        ))}
       </ul>
     </article>
   );
@@ -23,7 +30,9 @@ export async function getStaticProps() {
   const posts = await getAllPosts();
 
   return {
-    props: {},
+    props: {
+      posts: JSON.parse(JSON.stringify(posts)),
+    },
   };
 }
 

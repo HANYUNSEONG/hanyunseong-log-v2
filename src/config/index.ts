3abb24ad3060e0bb6ed0e44e18afef9fe410957c
HANYUNSEONG <hanyunseong.dev@gmail.com>
2023-03-29 00:01:24 +0900

    feat: favicon 추가, add meta description

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,8 @@
+import { DefaultSeoProps } from "next-seo";
+
 export const blogConfig = {
   title: "hanyunseong-log",
+  description: "hanyunseong-log",
   locale: "ko",
   url: "https://hanyunseong-log.dev",
   author: {
@@ -7,12 +10,33 @@ export const blogConfig = {
   },
 };
 
-export const defaultSEO = {
+export const defaultSEO: DefaultSeoProps = {
   title: blogConfig.title,
+  description: blogConfig.description,
   openGraph: {
     type: "website",
     locale: "ko-KR",
     url: blogConfig.url,
     siteName: blogConfig.title,
+    images: [
+      {
+        url: "/images/favicon.png",
+        width: 250,
+        height: 250,
+        alt: "hanyunseong-log Logo",
+        type: "image/png",
+      },
+    ],
   },
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "/images/favicon.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      href: "/images/favicon.png",
+      sizes: "76x76",
+    },
+  ],
 };
